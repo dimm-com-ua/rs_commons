6d70d6e41cd2dd8f1455cf29f0e90fdf63163a74
dimm-com-ua <59435056+dimm-com-ua@users.noreply.github.com>
2022-11-25 23:03:42 +0200

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@ pub mod domains;
 
 #[cfg(test)]
 pub mod tests {
+    use crate::domains::http_entities::api_response::ApiResponse;
+
     #[test]
-    fn test() {
-        assert_eq!(2+2, 4)
+    fn response_code() {
+        let resp_400 = ApiResponse::app_api_some_error("400");
+        assert_eq!(resp_400.status, 400)
     }
 }
